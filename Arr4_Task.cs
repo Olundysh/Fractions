cf0cd54317498785e0309bc6194d102f16805f9a
Vlad Kiselev <shiko-1st@yandex.ru>
2023-02-04 20:54:41 +0300

    Solution from internet

--- Arr4_Task.cs
+++ Arr4_Task.cs
@@ -1,81 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 /* превратить рациональную дробь a/b (0 < a < b < 100000) в десятичную   "1/6" => "0.1(6)" */
-namespace ConsoleApp1
+namespace ConsoleApp1;
+
+class Arr4_Task
 {
-    class Arr4_Task
+    public static string Division(int a, int b)
     {
-        public static string Division(int a, int b)
+        var res = "0.";
+        var remainders = new List<int>();
+        var i = 1;
+        remainders.Add(a);
+        while (remainders[i - 1] > 0)
         {
-            var remainOfDivision = a;
-            var growingString = "";
-            var endOfPeriod = 0;
-            var startOfPeriod = 0;
-            
-            for (var i = 0; i < 100; i++)
+            remainders.Add(0);
+            int x = remainders[i - 1] * 10;
+            int currentDigit = (x - x % b) / b;
+            remainders[i] = x % b;
+                
+            res += currentDigit;
+
+            for (var j = 0; j < i; j++)
             {
-                var resultOfDivision = 0;
-                
-                resultOfDivision = remainOfDivision * 10 / b;
-                
-                remainOfDivision = remainOfDivision * 10 % b;
-                growingString += resultOfDivision;
-                
-                if (i > 0)
-                {
-                    for (var j = 1; j < 100; j++)
-                    {
-                        resultOfDivision = remainOfDivision * 10 / b;
-                
-                        remainOfDivision = remainOfDivision * 10 % b;
-                        growingString += resultOfDivision;
-                        
-                        if (remainOfDivision == 0)
-                        {
-                            return "0," + growingString.TrimEnd('0');
-                        }
-                        
-                        if (growingString[i-1] == growingString[j] && growingString[i-1] != '0')
-                        {
-                            startOfPeriod = i-1;
-                            endOfPeriod = j;
-                            return "0," + growingString.Substring(0, startOfPeriod) + "(" +
-                                   growingString.Substring(startOfPeriod, endOfPeriod) + ")";
-                        } 
-                        
-                        if (growingString[i-1] == '0')
-                        {
-                            for (var k = 0; k < 100; k++)
-                            {
-                                resultOfDivision = remainOfDivision * 10 / b;
-                
-                                remainOfDivision = remainOfDivision * 10 % b;
-                                growingString += resultOfDivision;
-                        
-                        
-                                if (growingString[k] != '0')
-                                {
-                                    startOfPeriod = i-1;
-                                    endOfPeriod = k;
-                                    return "0," + growingString.Substring(0, startOfPeriod) + "(" +
-                                           growingString.Substring(startOfPeriod, endOfPeriod+1) + ")";
-                                }
-                            }
-                        }
-                    }
-                }
+                if (remainders[i] != remainders[j])
+                    continue;
+                res = res.Insert(j + 2, "(") + ")";
+                remainders[i] = 0;
             }
 
-            return "0," + growingString.Substring(0, startOfPeriod) + "(" +
-                   growingString.Substring(startOfPeriod, endOfPeriod+1) + ")";
+            i++;
         }
+
+        return res;
     }
 }
-    
-    
-   
-
-         
-
--- Program.cs
+++ Program.cs
@@ -1,94 +1,108 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace ConsoleApp1;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Console.WriteLine(Arr4_Task.Division(1, 99999));
+        // Console.WriteLine(Arr4_Task.Division(5, 7));
+        // Console.WriteLine(Arr4_Task.Division(1, 3));
+        // Console.WriteLine(Arr4_Task.Division(1, 33333));
+        // Console.WriteLine(Arr4_Task.Division(1, 6));
+        // Console.WriteLine(Arr4_Task.Division(1, 3535));
+        // Console.WriteLine(Arr4_Task.Division(5, 8));
+
+        bool err = false;
+        var rand = new Random((int) DateTime.Now.Millisecond);
+        for (int i = 0; i < 1000; i++)
         {
-               Console.WriteLine(Arr4_Task.Division(1,99999));
-    // bool err = false;
-    // var rand = new Random((int)DateTime.Now.Millisecond);
-    // for (int i = 0; i < 1000; i++)
-    // {
-    //     int b = rand.Next(1, 10000);
-    //     int a = rand.Next(1, b);
-    //     var actual = Arr4_Task.Division(a, b);
-    //     var convert = Convertion(actual);
-    //     var expect = TestDivision(a, b);
-    //     if (convert != expect)
-    //     {
-    //         Console.WriteLine("Error : {0}/{1}", a, b);
-    //         Console.WriteLine("Actual : {0}", actual);
-    //         Console.WriteLine("Expect : {0}", expect);
-    //         err = true;
-    //         break;
-    //     }
-    //     if (i % 100 == 0)
-    //     {
-    //         Console.Clear();
-    //         Console.WriteLine("Выполнено {0}%", i / 10);
-    //     }
-    // }
-    //     if (err == false)
-    //     {
-    //         Console.Clear();
-    //         Console.WriteLine("Все тесты пройдены. Задание выполнено!");
-    //     }
-    //     Console.ReadKey();
-    // }
-    // static string Convertion(string inputString)
-    // {
-    //     var maxVal = 10000;
-    //     var result = "0.";
-    //     int start = 0;
-    //     int lenght = 0;
-    //     var cycle = new int[maxVal];
-    //     for (int i = 2; i < inputString.Length; i++)
-    //     {
-    //         var number = inputString.Substring(i, 1);
-    //         if (number == "(")
-    //             start = i;
-    //         else if (number == ")")
-    //             break;
-    //         else
-    //         {
-    //             if (start > 0)
-    //             {
-    //                 lenght++;
-    //                 cycle[i - start - 1] = int.Parse(number);
-    //             }
-    //             result += number;
-    //         }
-    //     }
-    //     for (int i = result.Length, j = 0; i < maxVal; i++)
-    //     {
-    //         result += cycle[j];
-    //         j++;
-    //         if (j == lenght) j = 0;
-    //     }
-    //     return result;
-    // }
-    //
-    // static string TestDivision(int m, int n)
-    // {
-    // var maxVal = 10000;
-    // long q0 = m % n;
-    // long q = q0;
-    // int i = 2;
-    // long c = m / n;
-    // string result = c + ".";
-    // long len = maxVal;
-    // if (q == 0)
-    //     return result;
-    // while (i < len)
-    // {
-    //     long digit = (q * 10) / n;
-    //     result += digit;
-    //     i++;
-    //     q = (q * 10) % n;
-    // }
-    // return result;
-         }
+            int b = rand.Next(1, 10000);
+            int a = rand.Next(1, b);
+            var actual = Arr4_Task.Division(a, b);
+            var convert = Convertion(actual);
+            var expect = TestDivision(a, b);
+            if (convert != expect)
+            {
+                Console.WriteLine("Error : {0}/{1}", a, b);
+                Console.WriteLine("Actual : {0}", actual);
+                Console.WriteLine("Expect : {0}", expect);
+                err = true;
+                break;
+            }
+
+            if (i % 100 == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Выполнено {0}%", i / 10);
+            }
+        }
+
+        if (err == false)
+        {
+            Console.Clear();
+            Console.WriteLine("Все тесты пройдены. Задание выполнено!");
+        }
+
+        Console.ReadKey();
+    }
+
+    static string Convertion(string inputString)
+    {
+        var maxVal = 10000;
+        var result = "0.";
+        int start = 0;
+        int lenght = 0;
+        var cycle = new int[maxVal];
+        for (int i = 2; i < inputString.Length; i++)
+        {
+            var number = inputString.Substring(i, 1);
+            if (number == "(")
+                start = i;
+            else if (number == ")")
+                break;
+            else
+            {
+                if (start > 0)
+                {
+                    lenght++;
+                    cycle[i - start - 1] = int.Parse(number);
+                }
+
+                result += number;
+            }
+        }
+
+        for (int i = result.Length, j = 0; i < maxVal; i++)
+        {
+            result += cycle[j];
+            j++;
+            if (j == lenght) j = 0;
+        }
+
+        return result;
+    }
+
+    static string TestDivision(int m, int n)
+    {
+        var maxVal = 10000;
+        long q0 = m % n;
+        long q = q0;
+        int i = 2;
+        long c = m / n;
+        string result = c + ".";
+        long len = maxVal;
+        if (q == 0)
+            return result;
+        while (i < len)
+        {
+            long digit = (q * 10) / n;
+            result += digit;
+            i++;
+            q = (q * 10) % n;
+        }
+
+        return result;
     }
 }
